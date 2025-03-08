dfb4a295bd9e33cdf0a3ed9a7b699ff21e49f3a9
Andrew Cartwright <drew.cartwright@sunnystreet.com>
2025-03-07 17:54:08 -0700

    remove amazon

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -67,12 +67,7 @@ export class HeaderComponent implements OnInit{
       {
         href: 'https://www.pinterest.com/hjcartwright',
         faIcon: faPinterestP
-      },
-      {
-        href: 'https://www.amazon.com/stores/author/B0CVR6T3YC?ingress=0&visitId=dca68d10-7463-4ac6-b6d1-cfd2db04a78c',
-        faIcon: faAmazon
       }
-
     ]
   }
 }
